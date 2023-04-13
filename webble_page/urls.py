0752411a40ca06f9a8b461f26df361aef01acc1e
psySheep <emelianas@gmail.com>
2023-04-13 21:17:09 +0300

    front page update

--- webble_page/urls.py
+++ webble_page/urls.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', LoginView.as_view(), name='login'),
     path('sign_up/', views.sign_up, name='sign_up'),
 ]
