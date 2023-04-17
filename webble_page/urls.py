b1fe22c3e2e57782f311b0bb526106bed58a4aba
psySheep <emelianas@gmail.com>
2023-04-17 18:24:02 +0300

    new brach

--- webble_page/urls.py
+++ webble_page/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
     path('sign_up/', views.sign_up, name='sign_up'),
+    path('books', views.book_list, name='books'),
 ]
